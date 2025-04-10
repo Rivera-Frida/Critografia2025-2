be4645abbcf17022cd44664890ebbeaad10e0a27
Rivera-Frida <78118910+Rivera-Frida@users.noreply.github.com>
2025-04-09 20:44:01 -0600

    Add files via upload

---
+++ DES.c
@@ -0,0 +1,251 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define bit(x,i,n) (((x) >> ((n)-(i)-1)) & 1)
+#define toggle(x,i,n) ((x) ^= (1ULL << ((n)-(i)-1)))
+#define change(x,i,b,n) ((bit(x,i,n) != (b)) ? toggle(x,i,n) : (void)0)
+#define rotl(x,i,n) ((((x) << (i)) | ((x) >> ((n) - (i)))) & ((1ULL << (n)) - 1))
+#define rotr(x,i,n) ((((x) >> (i)) | ((x) << ((n) - (i)))) & ((1ULL << (n)) - 1))
+#define con(x,y,n) (((x) << (n)) | (y))
+#define rhalf(x,n) ((x) & ((1ULL << (n/2)) - 1))
+#define lhalf(x,n) ((x) >> (n/2))
+
+void printBinary(unsigned long long x, unsigned int n) {
+    for (int i = n - 1; i >= 0; i--) {
+        printf("%llu", (x >> i) & 1ULL);
+    }
+    printf("\n");
+}
+
+unsigned long long concat(unsigned long long x, unsigned long long y, int n) {
+    return ((x << n) | y);
+}
+
+unsigned long long IP(unsigned long long input) {
+    unsigned long long output = 0;
+    int IP[64] = {
+        58, 50, 42, 34, 26, 18, 10, 2,
+        60, 52, 44, 36, 28, 20, 12, 4,
+        62, 54, 46, 38, 30, 22, 14, 6,
+        64, 56, 48, 40, 32, 24, 16, 8,
+        57, 49, 41, 33, 25, 17, 9, 1,
+        59, 51, 43, 35, 27, 19, 11, 3,
+        61, 53, 45, 37, 29, 21, 13, 5,
+        63, 55, 47, 39, 31, 23, 15, 7
+    };
+
+    for (int i = 0; i < 64; i++) {
+        change(output, i, bit(input, IP[i] - 1, 64), 64);
+    }
+    return output;
+}
+
+unsigned long long IPInv(unsigned long long input) {
+    unsigned long long output = 0;
+    int IP_inv[64] = {
+        40, 8, 48, 16, 56, 24, 64, 32,
+        39, 7, 47, 15, 55, 23, 63, 31,
+        38, 6, 46, 14, 54, 22, 62, 30,
+        37, 5, 45, 13, 53, 21, 61, 29,
+        36, 4, 44, 12, 52, 20, 60, 28,
+        35, 3, 43, 11, 51, 19, 59, 27,
+        34, 2, 42, 10, 50, 18, 58, 26,
+        33, 1, 41, 9, 49, 17, 57, 25
+    };
+
+    for (int i = 0; i < 64; i++) {
+        change(output, i, bit(input, IP_inv[i] - 1, 64), 64);
+    }
+    return output;
+}
+
+unsigned long long PC1(unsigned long long input) {
+    unsigned long long output = 0;
+    int PC1[56] = {
+        57,49,41,33,25,17,9,
+        1,58,50,42,34,26,18,
+        10,2,59,51,43,35,27,
+        19,11,3,60,52,44,36,
+        63,55,47,39,31,23,15,
+        7,62,54,46,38,30,22,
+        14,6,61,53,45,37,29,
+        21,13,5,28,20,12,4
+    };
+
+    for (int i = 0; i < 56; i++) {
+        change(output, i, bit(input, PC1[i] - 1, 64), 56);
+    }
+    return output;
+}
+
+unsigned long long PC2(unsigned long long input) {
+    unsigned long long output = 0;
+    int PC2[48] = {
+        14,17,11,24,1,5,
+        3,28,15,6,21,10,
+        23,19,12,4,26,8,
+        16,7,27,20,13,2,
+        41,52,31,37,47,55,
+        30,40,51,45,33,48,
+        44,49,39,56,34,53,
+        46,42,50,36,29,32
+    };
+
+    for (int i = 0; i < 48; i++) {
+        change(output, i, bit(input, PC2[i] - 1, 56), 48);
+    }
+    return output;
+}
+
+unsigned long long expand(unsigned long long input) {
+    unsigned long long output = 0;
+    int E[48] = {
+        32,1,2,3,4,5,
+        4,5,6,7,8,9,
+        8,9,10,11,12,13,
+        12,13,14,15,16,17,
+        16,17,18,19,20,21,
+        20,21,22,23,24,25,
+        24,25,26,27,28,29,
+        28,29,30,31,32,1
+    };
+
+    for (int i = 0; i < 48; i++) {
+        change(output, i, bit(input, E[i] - 1, 32), 48);
+    }
+    return output;
+}
+
+unsigned long long P(unsigned long long input) {
+    unsigned long long output = 0;
+    int P[32] = {
+        16,7,20,21,29,12,28,17,
+        1,15,23,26,5,18,31,10,
+        2,8,24,14,32,27,3,9,
+        19,13,30,6,22,11,4,25
+    };
+
+    for (int i = 0; i < 32; i++) {
+        change(output, i, bit(input, P[i] - 1, 32), 32);
+    }
+    return output;
+}
+
+unsigned long long* keygen(unsigned long long input) {
+    unsigned long long* output = malloc(16 * sizeof(unsigned long long));
+    unsigned long long C, D;
+    int shifts[] = {1, 1, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 1};
+
+    unsigned long long PC1key = PC1(input);
+    C = lhalf(PC1key, 56);
+    D = rhalf(PC1key, 56);
+
+    for (int i = 0; i < 16; i++) {
+        C = rotl(C, shifts[i], 28);
+        D = rotl(D, shifts[i], 28);
+        output[i] = PC2(concat(C, D, 28));
+    }
+    return output;
+}
+
+unsigned long long Sbox(unsigned long long input, int n) {
+    static int S[8][4][16] = {
+    {
+        {14,4,13,1,2,15,11,8,3,10,6,12,5,9,0,7},
+        {0,15,7,4,14,2,13,1,10,6,12,11,9,5,3,8},
+        {4,1,14,8,13,6,2,11,15,12,9,7,3,10,5,0},
+        {15,12,8,2,4,9,1,7,5,11,3,14,10,0,6,13}
+    },
+    {
+        {15,1,8,14,6,11,3,4,9,7,2,13,12,0,5,10},
+        {3,13,4,7,15,2,8,14,12,0,1,10,6,9,11,5},
+        {0,14,7,11,10,4,13,1,5,8,12,6,9,3,2,15},
+        {13,8,10,1,3,15,4,2,11,6,7,12,0,5,14,9}
+    },
+    {
+        {10,0,9,14,6,3,15,5,1,13,12,7,11,4,2,8},
+        {13,7,0,9,3,4,6,10,2,8,5,14,12,11,15,1},
+        {13,6,4,9,8,15,3,0,11,1,2,12,5,10,14,7},
+        {1,10,13,0,6,9,8,7,4,15,14,3,11,5,2,12}
+    },
+    {
+        {7,13,14,3,0,6,9,10,1,2,8,5,11,12,4,15},
+        {13,8,11,5,6,15,0,3,4,7,2,12,1,10,14,9},
+        {10,6,9,0,12,11,7,13,15,1,3,14,5,2,8,4},
+        {3,15,0,6,10,1,13,8,9,4,5,11,12,7,2,14}
+    },
+    {
+        {2,12,4,1,7,10,11,6,8,5,3,15,13,0,14,9},
+        {14,11,2,12,4,7,13,1,5,0,15,10,3,9,8,6},
+        {4,2,1,11,10,13,7,8,15,9,12,5,6,3,0,14},
+        {11,8,12,7,1,14,2,13,6,15,0,9,10,4,5,3}
+    },
+    {
+        {12,1,10,15,9,2,6,8,0,13,3,4,14,7,5,11},
+        {10,15,4,2,7,12,9,5,6,1,13,14,0,11,3,8},
+        {9,14,15,5,2,8,12,3,7,0,4,10,1,13,11,6},
+        {4,3,2,12,9,5,15,10,11,14,1,7,6,0,8,13}
+    },
+    {
+        {4,11,2,14,15,0,8,13,3,12,9,7,5,10,6,1},
+        {13,0,11,7,4,9,1,10,14,3,5,12,2,15,8,6},
+        {1,4,11,13,12,3,7,14,10,15,6,8,0,5,9,2},
+        {6,11,13,8,1,4,10,7,9,5,0,15,14,2,3,12}
+    },
+    {
+        {13,2,8,4,6,15,11,1,10,9,3,14,5,0,12,7},
+        {1,15,13,8,10,3,7,4,12,5,6,11,0,14,9,2},
+        {7,11,4,1,9,12,14,2,0,6,10,13,15,3,5,8},
+        {2,1,14,7,4,10,8,13,15,12,9,0,3,5,6,11}
+    }
+};
+
+
+    int row = (bit(input, 0, 6) << 1) | bit(input, 5, 6);
+    int col = (bit(input, 1, 6) << 3) | (bit(input, 2, 6) << 2) | (bit(input, 3, 6) << 1) | bit(input, 4, 6);
+    return S[n][row][col];
+}
+
+unsigned long long feistel(unsigned long long Rn, unsigned long long Kn) {
+    unsigned long long B = expand(Rn) ^ Kn;
+    unsigned long long output = 0;
+
+    for (int i = 0; i < 8; i++) {
+        unsigned long long block = (B >> (42 - 6 * i)) & 0x3F;
+        output = (output << 4) | Sbox(block, i);
+    }
+
+    return P(output);
+}
+
+unsigned long long DES(unsigned long long input, unsigned long long* keys) {
+    input = IP(input);
+    unsigned long long L = lhalf(input, 64);
+    unsigned long long R = rhalf(input, 64);
+
+    for (int i = 0; i < 16; i++) {
+        unsigned long long temp = R;
+        R = L ^ feistel(R, keys[i]);
+        L = temp;
+    }
+
+    unsigned long long pre_output = concat(R, L, 32);
+    return IPInv(pre_output);
+}
+
+int main() {
+    unsigned long long key = 0x666AB366AB366AB3;
+    unsigned long long text_bin = 0;
+    unsigned long long* keys = keygen(key);
+
+    char mensaje[] = "testtext";
+    for (int i = 0; i < 8; i++) {
+        text_bin = (text_bin << 8) | mensaje[i];
+    }
+
+    unsigned long long encrypted = DES(text_bin, keys);
+    printBinary(encrypted, 64);
+
+    free(keys);
+    return 0;
+}
